4a2564bba433bdce6afae8831033d1187b4ac0ca
Nader <asadi.nader97@gmail.com>
2020-06-06 18:17:24 +0430

    hello-flask completed. However, some modifications and documentations is still required.

---
+++ token-refreshing/resources/user.py
@@ -0,0 +1,96 @@
+from flask_restful import Resource, reqparse
+from werkzeug.security import safe_str_cmp
+from flask_jwt_extended import (
+    jwt_required,
+    create_access_token, 
+    create_refresh_token, 
+    jwt_refresh_token_required, 
+    get_jwt_identity,
+    get_raw_jwt)
+
+from models.user import UserModel
+from blacklist import BLACKLIST
+
+
+_user_parser = reqparse.RequestParser()
+_user_parser.add_argument('username',
+                    type=str,
+                    required=True,
+                    help="This field cannot be blank."
+                    )
+_user_parser.add_argument('password',
+                    type=str,
+                    required=True,
+                    help="This field cannot be blank."
+                    )
+
+
+class UserRegister(Resource):
+    def post(self):
+        data = _user_parser.parse_args()
+
+        if UserModel.find_by_username(data['username']):
+            return {"message": "A user with that username already exists"}, 400
+
+        user = UserModel(**data)
+        user.save_to_db()
+
+        return {"message": "User created successfully."}, 201
+
+
+class User(Resource):
+    """
+    This Resource(API) is created to get and delete users during delevopment time.
+    """
+    @classmethod
+    def get(cls, user_id):
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {'message': 'User not found.'}, 404
+        return user.json()
+
+    @classmethod
+    def delete(cls, user_id):
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {'message': 'User not found.'}, 404
+
+        user.delete_from_db()
+        return {'message': 'User deleted.'}, 200
+
+
+class UserLogin(Resource):
+    @classmethod
+    def post(cls):
+        data = _user_parser.parse_args()
+
+        user = UserModel.find_by_username(data['username'])
+
+        if user and safe_str_cmp(user.password, data['password']):
+            access_token = create_access_token(identity=user.id, fresh=True)
+            refresh_token = create_refresh_token(user.id)
+            return{
+                'access_token': access_token,
+                'refresh_token': refresh_token
+            }, 200
+        
+        return {'message': 'Invalid credentials'}, 401
+
+
+class UserLogout(Resource):
+    @jwt_required
+    def post(self):
+        # We will blacklist user token when user loges out. We will use jwt token_id to do so.
+        jti = get_raw_jwt()['jti'] # jti is "JWT ID", a unique indentifier for a JWT
+        BLACKLIST.add(jti)
+        return {'message': 'Successfully logged out.'}, 200
+
+
+class TokenRefresh(Resource):
+    @jwt_refresh_token_required
+    def post(self):
+        # If request does not contain a refresh token flask-JWT-Extended will return an error
+        current_user = get_jwt_identity()
+        new_token = create_access_token(identity=current_user, fresh=False) # fresh=False means we have refreshed token and user has not logged in manually
+        return {'access_token': new_token}, 200
+
